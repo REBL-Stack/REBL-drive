e2a9a7b2d20fd9eaacad37d50215e2522f72c343
Terje Norderhaug <terje@in-progress.com>
2019-10-21 23:20:56 -0700

    Style sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHdd, faStar, faShare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-import css from "./Sidebar.css"
+import "./Sidebar.css"
 
 export default function Sidebar (props) {
   return (
     <nav id="sidebar" className="bg-light collapse navbar-collapse d-sm-block">
       <div id="sidebarContent" className="">
         <ul className="list-unstyled components">
-          <li><NavLink to={"/drive"}>My Drive</NavLink></li>
-          <li><NavLink to={"/favorites"}>Starred</NavLink></li>
-          <li><NavLink to={"/shared"}>Shared</NavLink></li>
-          <li><NavLink to={"/trash"}>Trash</NavLink></li>
+          <li><NavLink to={"/drive"}>
+                <FontAwesomeIcon icon={faHdd}/> My Drive
+              </NavLink>
+          </li>
+          <li><NavLink to={"/favorites"}>
+                 <FontAwesomeIcon icon={faStar}/>Favorites
+               </NavLink>
+          </li>
+          <li><NavLink to={"/shared"}>
+                 <FontAwesomeIcon icon={faShare}/>Shared
+               </NavLink>
+           </li>
+          <li><NavLink to={"/trash"}>
+                <FontAwesomeIcon icon={faTrash}/>Trash
+              </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
