4dcd88fa0a0421aa100c3ec0a081a613056e424b
Terje Norderhaug <terje@in-progress.com>
2019-11-07 15:52:31 -0800

    Add unique key to breadcrumb steps

--- src/library/Breadcrumb.js
+++ src/library/Breadcrumb.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 export default function Breadcrumb ({trail, onClick, title}) {
+  const goTop = (trail.length > 0) && (() => onClick({path: []}))
   return(
     <nav aria-label="breadcrumb">
       <ul className="breadcrumb">
         {title &&
-         <li className="breadcrumb-item breadcrumb-title">
-           <a href="#" onClick={ () => onClick({path: []})}>{title}</a>
+         <li key="0" className="breadcrumb-item breadcrumb-title">
+           { goTop ? <a href="#" onClick={ goTop }>{title}</a> : title}
          </li>}
         {[...Array(trail.length).keys()].map( n =>
-          <li className="breadcrumb-item">
-            {(n < trail.length - 1) ? <a href="#" onClick={() => onClick({path: trail.slice(0, n+1)})}>{trail[n]}</a> : trail[n]}
+          <li key={n+1} className="breadcrumb-item">
+            {(n < trail.length - 1) ?
+              <a href="#" onClick={() => onClick({path: trail.slice(0, n+1)})}>{trail[n]}</a>
+              : trail[n]}
           </li>)}
       </ul>
     </nav>
